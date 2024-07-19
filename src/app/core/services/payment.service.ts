82f0fbb5251270b4fd6830306de729ead7280f00
Danil Hatin <kipsa25@mail.ru>
2024-07-20 00:45:13 +0500

    payment

---
+++ src/app/core/services/payment.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, finalize, Observable, tap, throwError } from 'rxjs';
+
+interface LabeledPrice {
+    label: string,
+    amount: number,
+}
+interface Message {
+    message_id: number,
+    date: number,
+    chat: {id: number, type: string},
+}
+
+interface InvoiceData {
+    chat_id: number | string,
+    message_thread_id?: number,
+    title: string,
+    description: string,
+    payload: string,
+    provider_token?: string,
+    currency: string,
+    prices: Array<LabeledPrice>,
+    max_tip_amount?: number,
+    suggested_tip_amounts?: Array<number>,
+    start_parameter?: string,
+}
+@Injectable({
+    providedIn: 'root',
+})
+
+
+export class PaymentService {
+    private apiUrl = "https://api.telegram.org/bot5655959098:AAE1sXjgPON7tGhQGetT9UiDeI6FQccAInI" // + <token>/method
+
+    constructor(private http: HttpClient) { }
+
+    sendInvoice(userData: InvoiceData): Observable<any> {
+       return this.http.post<Message>(this.apiUrl + "/sendInvoice", userData).pipe(
+            tap(response => {
+                if(response) {
+                    console.log("Payment success!!");
+                }
+            }),
+            finalize(() => { console.log("Payment finallize"); }),
+            catchError((error: HttpErrorResponse) => { 
+                console.log("Payment closed!");
+                return throwError(() => error) 
+            }),
+        )
+    }
+}
--- src/app/features/ranking/components/ranking-top/ranking-top.component.ts
+++ src/app/features/ranking/components/ranking-top/ranking-top.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { PaymentService } from 'src/app/core/services/payment.service';
 
+interface LabeledPrice {
+  label: string,
+  amount: number,
+}
+interface InvoiceData {
+  chat_id: number | string,
+  message_thread_id?: number,
+  title: string,
+  description: string,
+  payload: string,
+  provider_token?: string,
+  currency: string,
+  prices: Array<LabeledPrice>,
+  max_tip_amount?: number,
+  suggested_tip_amounts?: Array<number>,
+  start_parameter?: string,
+}
 @Component({
   selector: 'app-ranking-top',
   templateUrl: './ranking-top.component.html',
   styleUrls: ['./ranking-top.component.scss']
 })
-export class RankingTopComponent implements OnInit {
 
-  constructor() { }
+
+export class RankingTopComponent implements OnInit {
+  
+
+  paymentData: InvoiceData = {
+    chat_id: '1231',
+    title: 'buy 100 stars',
+    description: 'buy 100',
+    payload: 'load',
+    currency: 'XTR',
+    prices: [{label: "one", amount: 100}]
+  }
+  constructor(private paymentService: PaymentService) { }
 
   ngOnInit() {
+  }
+
+
+  pay(paymentData: InvoiceData) {
+    this.paymentService.sendInvoice(paymentData).subscribe(response => {console.log(response)});
   }
 
 }
