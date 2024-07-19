d3184d2d502b985b0966be6df9f0ea666e5abb37
Danil Hatin <kipsa25@mail.ru>
2024-07-20 01:58:24 +0500

    payment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { TelegramService } from './core/services/telegram.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,14 @@ import { TelegramService } from './core/services/telegram.service';
 })
 export class AppComponent {
   telegram = inject(TelegramService);
-  constructor() {
+  chatId: string | null = null;
+
+  constructor(private route: ActivatedRoute) {
     this.telegram.ready();
+    this.route.queryParams.subscribe(params => {
+      this.chatId = params['chat_id'];
+      console.log('Chat ID:', this.chatId);
+      // Теперь вы можете использовать chatId в вашем приложении
+    });
   }
 }
